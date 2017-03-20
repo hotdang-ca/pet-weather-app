72fa5e98320674d1e22f388f6cf8ac8574f22e10
James Robert Perih <james@hotdang.ca>
2017-03-20 15:16:47 -0500

    welcome screen that lists pets in a side-scrolling window and all related components and style requirements

--- src/components/screens/WelcomeScreen.js
+++ src/components/screens/WelcomeScreen.js
@@ -1,31 +1,92 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-import { PrimaryButton } from '../common';
+import { PrimaryButton, PetDetailsCard } from '../common';
 
 class WelcomeScreen extends Component {
   constructor(props) {
     super(props);
 
-    this.handleGetHelpClicked = this.handleGetHelpClicked.bind(this);
+    this.state = {
+      pets: [],
+      isLoading: false
+    };
+
+    this.handleTrackNewPetClicked = this.handleTrackNewPetClicked.bind(this);
+    this.handlePetClicked = this.handlePetClicked.bind(this);
+    this.refreshPets = this.refreshPets.bind(this);
+    this.renderPets = this.renderPets.bind(this);
   }
 
   componentWillMount() {
+    this.refreshPets();
   }
 
-  handleGetHelpClicked(clickEvent) {
-    clickEvent.preventDefault();
-    // browserHistory.push('/service');
+  handleTrackNewPetClicked(event) {
+    event.preventDefault();
+    console.log("new pet");
+  }
+
+  handlePetClicked(id, component) {
+    console.log('details for pet', id);
+    this.context.router.history.push(`/pets/${id}`);
+  }
+
+  refreshPets() {
+    this.setState({
+      isLoading: true
+    });
+
+    axios.get('//pet-shelter-api.herokuapp.com/pets')
+    .then((response) => {
+      this.setState({
+        isLoading: false,
+        pets: response.data
+      });
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });
+  }
+
+  renderPets() {
+    const { pets } = this.state;
+
+    return pets.map((pet, index) => {
+      console.log(pet);
+      return (<PetDetailsCard key={index} pet={pet} onButtonClicked={this.handlePetClicked.bind(this, pet.id)} />);
+    });
   }
 
   render() {
+    const { isLoading } = this.state;
+
     return (
       <div id='WelcomeScreen'>
         <div className='container'>
           <h2>Does my pet need an umbrella?</h2>
+          <div className='pets-list'>
+            { isLoading
+              ? <em>Loading..</em>
+              : this.renderPets()
+            }
+          </div>
         </div>
+
+        <PrimaryButton
+          title='Track a New Pet'
+          additionalStyles={{
+            marginTop: '64px'
+          }}
+          onClick={this.handleTrackNewPetClicked}
+        />
       </div>
     );
   }
 }
+
+WelcomeScreen.contextTypes = {
+    router: React.PropTypes.object
+};
 
 export { WelcomeScreen };
