5eb9952428f2e83db3f11ae1c1e1ca05ee4643ac
James Robert Perih <james@hotdang.ca>
2017-03-20 15:32:12 -0500

    initial details flow

--- src/components/screens/PetDetailsScreen.js
+++ src/components/screens/PetDetailsScreen.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PetDetailsScreen extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      pet: {
+        typeId: undefined,
+        breedId: undefined
+      }
+    };
+
+    this.refreshPetDetails = this.refreshPetDetails.bind(this);
+  }
+
+  componentDidMount() {
+    this.refreshPetDetails();
+  }
+
+  refreshPetDetails() {
+    const { match } = this.props;
+    const { petId } = match.params;
+
+    axios.get(`//pet-shelter-api.herokuapp.com/pets/${petId}`)
+    .then((response) => {
+      console.log(response.data);
+
+      this.setState({
+        pet: response.data
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
-    const { match } = this.props;
-    const { petId } = match.params;
-    console.log(match);
-    
+    const { name, typeId, breedId } = this.state.pet;
+
     return (
       <div id='PetDetailsScreen'>
         <div className='container'>
-          Hello Pet Details for { petId || 'no pet in particular'}
+          Hello Pet Details for { name } which is a { typeId } { breedId }
         </div>
       </div>
     );
